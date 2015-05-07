f192fd92939c8641de8c6062a6ab515a41682a06
Carlos Latorre <carlos.latorre.sanchez@gmail.com>
2015-05-08 00:18:50 +0200

    Añadido recepcion datos desde Arduino

---
+++ webadmin/control/temp.php
@@ -0,0 +1,17 @@
+<?php
+include '../db/db.php';
+
+
+$temperatura1= $_GET['temp1'];//Guardamos el parametro que han introducido mediante un GET
+$humedad1= $_GET['hume1'];
+$temperatura2= $_GET['temp2'];
+$humedad2= $_GET['hume2'];
+
+
+$sql1="INSERT INTO sensores(sensor,temp,hume) VALUES ('1',$temperatura1,$humedad1);";
+$datos=mysql_query($sql1,$con); //enviar cóo MySQL
+
+$sql2="INSERT INTO sensores(sensor,temp,hume) VALUES ('2',$temperatura1,$humedad1);";
+$datos2=mysql_query($sql2,$con);
+mysql_close($con);//cerrar conexion
+?>
